38c8374d58d86dfa904a51f94ad7696b913d93de
Joao Trindade <trindade.joao@gmail.com>
2010-03-26 02:41:26 +0000

    Starting to fetch the citations

--- parser.py
+++ parser.py
@@ -16,6 +16,7 @@ class ConferenceListParser(HTMLParser):
         self.in_a = False
 
     def handle_starttag(self, tag, attrs):
+        print tag
         if tag == "li" and attrs[0][0] == "class" and attrs[0][1] == 'small-text': 
             self.in_small = True
 
@@ -23,10 +24,11 @@ class ConferenceListParser(HTMLParser):
             self.in_acro = True
 
         if self.in_small and tag == 'a':
-            self.present_conference['link'] = attrs[0][1]
+            self.present_conference['link'] = 'http://portal.acm.org/' + attrs[0][1]
             self.in_a = True
 
     def handle_data(self, text):
+        print text
         if self.in_acro:
             self.present_conference['acro'] = text[:-1]
         if self.in_a:
@@ -45,6 +47,35 @@ class ConferenceListParser(HTMLParser):
         if self.in_a == True and tag == 'a':
             self.in_a = False
 
+
+class SingleConferenceParser(HTMLParser):
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.in_table = False
+        self.in_row = False
+        self.current_row = {}
+        self.total = {}
+
+    def handle_starttag(self, tag, attrs):
+        if tag == "table" and attrs[0][0] == "align" and attrs[2][1] == '5':
+            print "In table"
+            self.in_table = True
+
+        if self.in_table and tag == "td" and (attrs[0][1] == '#fff7e7' or attrs[0][1] == '#fff7e7'):
+            print "In row"
+            self.in_row = True
+
+
+    def handle_data(self, text):
+        if self.in_row:
+            print text
+
+    def handle_endtag(self, tag):
+        if self.in_table and tag == "table":
+            self.in_table = False
+            self.in_row = False
+
+
 ###############################
 
 def parse_conference_list():
@@ -53,11 +84,20 @@ def parse_conference_list():
 
     import urllib
     web_site = urllib.urlopen(url_string)
-    print web_site
     parser.feed(web_site.read())
-    print parser.conferentes
 
-def parse_conference_page():
-    pass
+    return parser.conferentes
 
-parse_conference_list()
+def parse_conference(conference_url):
+    parser = ConferenceListParser()
+
+    import urllib
+    print conference_url
+    web_site = urllib.urlopen(conference_url['link'])
+    parser.feed(web_site.read())
+
+conference_urls = parse_conference_list()
+
+for url in conference_urls:
+    parse_conference(url)
+
